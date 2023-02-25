53dcb8ad6b603003f8e04a28a4f93f8fb4798cc4
Arhises <roma1039@gmail.com>
2023-02-25 17:52:48 +0200

    Third q added

---
+++ Question_03/Program.cs
@@ -0,0 +1,52 @@
+﻿// Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+
+int[,,] CreateIntTreaD()
+{
+    Console.Write("Lines: ");
+    int line = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Colons: ");
+    int colon = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Deep: ");
+    int deep = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine();
+    int[,,] treaD = new int[line, colon, deep];
+    return treaD;
+}
+int[,,] FillTreaD(int[,,] treaD)
+{
+    int input = 10;
+    for (int i = 0; i < treaD.GetLength(0); i++)
+    {
+        for (int j = 0; j < treaD.GetLength(1); j++)
+        {
+            for (int k = 0; k < treaD.GetLength(2); k++)
+            {
+                treaD[i, j, k] = input;
+                input++;
+            }
+        }
+    }
+    return treaD;
+}
+void PrintTreaD(int[,,] treaD)
+{
+    for (int i = 0; i < treaD.GetLength(0); i++)
+    {
+        for (int j = 0; j < treaD.GetLength(1); j++)
+        {
+            for (int k = 0; k < treaD.GetLength(2); k++)
+            {
+                Console.Write($"[{i},{j},{k}] = {treaD[i, j, k]} ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] treaD = CreateIntTreaD();
+FillTreaD(treaD);
+PrintTreaD(treaD);
