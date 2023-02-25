e597e6c61067f1b5eded0178950b2b332e54a5e6
Arhises <roma1039@gmail.com>
2023-02-25 13:41:38 +0200

    First q added

---
+++ Question_01/Program.cs
@@ -0,0 +1,64 @@
+﻿// Найти произведение двух матриц
+
+int[,] CreateIntTable()
+{
+    Console.Write("Lines: ");
+    int line = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Colons: ");
+    int colon = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine();
+    int[,] table = new int[line, colon];
+    return table;
+}
+int[,] FillTable(int[,] table)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return table;
+}
+void PrintTable(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(" " + table[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+bool EqualMatrixes(int[,] first, int[,] second) => first.GetLength(0) == second.GetLength(0) && first.GetLength(1) == second.GetLength(1);
+void MultiplyTwoMatrixes(int[,] first, int[,] second)
+{
+    if (EqualMatrixes(first,second))
+    {
+        for (int i = 0; i < first.GetLength(0); i++)
+        {
+            for (int j = 0; j < first.GetLength(1); j++)
+            {
+                Console.Write($"{first[i,j] *second[i,j]} ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+int[,] first = CreateIntTable();
+int[,] second = CreateIntTable();
+
+FillTable(first);
+FillTable(second);
+
+PrintTable(first);
+Console.WriteLine();
+PrintTable(second);
+
+MultiplyTwoMatrixes(first,second);
